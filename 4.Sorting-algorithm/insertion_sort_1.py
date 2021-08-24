fa184ad5e4ea9231405c5b2ff78d5b82934056d2
hwiju yeom <yeom456@gmail.com>
2021-08-24 20:04:57 +0900

    8/24
    정렬 알고리즘

---
+++ 4.Sorting-algorithm/insertion_sort_1.py
@@ -0,0 +1,9 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Yeomyaloo/AppData/Local/Programs/Python/Python39/selection_sort_1.py
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+>>> 
+= RESTART: C:/Users/Yeomyaloo/AppData/Local/Programs/Python/Python39/insertion sort.py
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+>>> 
